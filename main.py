1dfd53c1d61f995712b056abd18cdb8fb1b19b92
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-12-01 21:42:00 -0600

    Update main.py
    ---

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = fastapi.FastAPI()
 
 origins = [
     "http://127.0.0.1:5000",
-    "https://shm-frontend-c3f2dc0fa89c.herokuapp.com"
+    "https://frontend-contactos-47896c7caf37.herokuapp.com"
 ]
 
 app.add_middleware(
